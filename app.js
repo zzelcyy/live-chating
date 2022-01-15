8b0cabfbc5e85cb4c4474b58c7bd05311568ea35
zzelcyy <celee@madup.com>
2022-01-15 13:43:42 +0900

    ✨ create static files

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 const express = require('express')
 const socket = require('socket.io')
 const http = require('http')
@@ -6,9 +7,19 @@ const app = express()
 const server = http.createServer(app)
 const io = socket(server)
 
+app.use('/css', express.static('./static/css'))
+app.use('/js', express.static('./static/js'))
+
 app.get('/', (req, res) => {
-    console.log('connected: "/"')
-    res.send('Hello')
+    fs.readFile('./static/index.html', (err, data) => {
+        if(err) {
+            res.send('error')
+        } else {
+            res.writeHead(200, {'Content-Type': 'text/html'})
+            res.write(data)
+            res.end()
+        }
+    })
 })
 
 server.listen(8080, () => {
